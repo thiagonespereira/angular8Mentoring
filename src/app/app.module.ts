2b1dcfa5f70da8ac6062f93f94d4cdf9c0b71119
Thiago Pereira <thiago.pereira@emailonacid.com>
2020-02-17 11:09:15 -0300

    Removing unused components of this study.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ParentComponent } from './components/parent/parent.component';
-import { ChildComponent } from './components/child/child.component';
 import { Parent2Component } from './components/parent2/parent2.component';
 import { Child2Component } from './components/child2/child2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ParentComponent,
-    ChildComponent,
     Parent2Component,
     Child2Component
   ],
